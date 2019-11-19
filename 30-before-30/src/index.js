a2dc45f357c372e3ce26d2577c6597b4c55c9b1f
Daniel <danstad2012@gmail.com>
2019-11-19 08:21:43 -0800

    start tues

--- 30-before-30/src/index.js
+++ 30-before-30/src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router} from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from './redux/store'
 import './index.css';
+import * as serviceWorker from './serviceWorker';
 import App from './App';
 
 ReactDOM.render(
--- 30-before-30/src/redux/store.js
+++ 30-before-30/src/redux/store.js
@@ -1,10 +1,10 @@
-import { createStore, applyMiddleWare } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import logger from 'redux-logger'
 
 import rootReducer from './root-reducer'
 
 const middleWares = [logger];
 
-const store = createStore(rootReducer, applyMiddleWare(...middleWares))
+const store = createStore(rootReducer, applyMiddleware(...middleWares))
 
 export default store;
