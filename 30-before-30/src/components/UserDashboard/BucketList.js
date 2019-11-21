4c571b57a73d339d21d30ef2ef1391e7c89078cf
Daniel <danstad2012@gmail.com>
2019-11-21 10:23:04 -0800

    display list items

--- 30-before-30/src/components/UserDashboard/BucketList.js
+++ 30-before-30/src/components/UserDashboard/BucketList.js
@@ -1,10 +1,40 @@
 import React, { useState, useContext, useEffect } from 'react';
 import ItemsContext from '../../contexts/items/items.contexts'
+import axiosWithAuth from '../../utils/axiosWithAuth';
 
 
 const BucketList = (props) => {
-  const ItemList = useContext(ItemsContext)
   //const Item = Items[match.params.userId]
+  const [bucket, setBucket] = useState([])
+  const id = 2
+
+  useEffect(() => {
+    const getBuckets = () => {
+      axiosWithAuth()
+    .get(`bucketlists/${id}/`)
+    .then(res => {
+      console.log(res.data);
+      setBucket(res.data)
+    })
+    .catch(err => console.log(err));
+    }
+    getBuckets();
+  }, []);
+
+
+
+
+  // axiosWithAuth()
+  //   .get(`bucketlists/${id}/`)
+  //   .then(res => {
+  //     console.log(res.data);
+  //     setBucket(res.data)
+  //   },[])
+  //   .catch(err => console.log(err));
+  console.log(bucket)
+      
+  const ItemList = useContext(bucket)
+
   console.log("Bucketlist", ItemList)
     const [list, setList] = useState([]);
 
@@ -12,7 +42,7 @@ const BucketList = (props) => {
     
     return (
         <ul>
-        {ItemList.map(p => (
+        {bucket.map(p => (
           <li className="card" key={p.name}>
             {p.description}
           </li>
