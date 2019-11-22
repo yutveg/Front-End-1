84d4c9c4488ede905e3b93d4b1d644a083b7b304
Daniel <danstad2012@gmail.com>
2019-11-22 10:52:07 -0800

    home page work around

--- 30-before-30/src/App.js
+++ 30-before-30/src/App.js
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect, useContext} from 'react';
+import Context, {Provider} from './contexts/context'
+import axios from 'axios';
+
 import Modal from './components/Modal';
 import { Switch, Link, Route, Redirect } from 'react-router-dom'
 //import CurrentUserContext from './contexts/current-user/current-user.context'
@@ -10,33 +13,51 @@ import UserDash from './components/UserDashboard/UserDash.js'
 import { SignUpPage } from './pages/SignUpPage';
 //import { Footer } from './components/Footer';
 import decode from 'jwt-decode'
-import { Provider } from './contexts/context';
 
 
 function App(props) {
-  //console.log(props)
-  //const [userData, setUserData] = useState([]);
+  const context = useContext(Context) 
+  const [bucket, setBucket] = useState([context.ITEM_DATA])
+
+  useEffect(() => {
+    axios
+      .post('https://project-30-before-30.herokuapp.com/api/auth/login', {username: "blue", password: "blue"})
+      .then(res => {
+        //console.log(values)
+        const token = res.data.token
+        console.log(token);
+        localStorage.removeItem('token')
+        localStorage.removeItem('username')
+        localStorage.setItem('token', res.data.token)
+        // localStorage.setItem('username', username)
+        localStorage.setItem('userId', decode(token).id)
+      })
+      .catch(err => {console.log(err)});
+  }, [])
   
   const token  = localStorage.getItem('token')
   const decoded = decode(token)
 
 
   return (
-    <div className="App">  
-      <Nav /> 
-      <Link to={`/users/${decoded.id}`}>USER DASH</Link>
-      <Route path='/home' component={HomePage} />
-      <Switch>
-        <Route path="/home/:id" render={props => <Modal {...props} /> } /> 
-        <Route path={`/users/${decoded.id}`} component={UserDash} />
-        <Route exact path='/sign-in' render={()=>props.currentUser
-          ?(<Redirect tp='/' />)
-          :<SignInPage />} />
-        <Route path='/sign-up' component={SignUpPage} />
-        <Route path="/USERBUCKETLIST/:id" component={Modal} />
-      </Switch>
-      {/*<Footer /> */}
-    </div>
+    <Provider value={{bucket}}>
+      <div className="App">
+        <Nav /> 
+        <Link to={`/users/${decoded.id}`}>USER DASH</Link>
+        <Route path='/home' component={HomePage} />
+        <Switch>
+          <Route path="/home/:id" render={props => <Modal {...props} /> } /> 
+          <Route path={`/users/${decoded.id}`} component={UserDash} />
+          <Route exact path='/sign-in' render={()=>props.currentUser
+            ?(<Redirect tp='/' />)
+            :<SignInPage />} />
+          <Route path='/sign-up' component={SignUpPage} />
+          <Route path="/USERBUCKETLIST/:id" component={Modal} />
+        </Switch>
+        {/*<Footer /> */}
+      </div>
+    </Provider>
+
   );
 }
 export default App;
