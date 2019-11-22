fc9577e610d41e9be4273058fcc8ab2be465a442
hsisco <hysensisco@gmail.com>
2019-11-21 17:05:34 -0800

    resturctured HomePage

--- 30-before-30/src/pages/HomePage.jsx
+++ 30-before-30/src/pages/HomePage.jsx
@@ -6,14 +6,15 @@ import { HomeTile } from '../components/HomeTile';
 
 const useStyles = makeStyles(theme => ({
   root: {
-    // display: 'flex',
-    // flexWrap: 'wrap',
-    // justifyContent: 'space-around',
-    // overflow: 'hidden',
     backgroundColor: theme.palette.background.paper,
   },
-  gridList: {
-
+  tileTitle: {
+    textAlign: 'center',
+    letterSpacing: '2px',
+    color: '#8F573B',
+    fontWeight: '700',
+    fontSize: '1.5em',
+    marginBottom: 0,
   }
 }));
 
@@ -22,11 +23,11 @@ export function HomePage() {
   
   return (
     <div className={classes.root}>
-      Welcome to the Home Page
+      <h2 className={classes.tileTitle}>
+        CHECK OUT SOME COOL LISTS
+      </h2>
       <GridList cellHeight={180} className={classes.gridList}>
-        {/* <GridListTile key="Subheader" cols={2} style={{ height: 'auto' }}> */}
-          <HomeTile />
-        {/* </GridListTile> */}
+        <HomeTile />
       </GridList>
     </div>
   );
